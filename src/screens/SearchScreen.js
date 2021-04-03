f2e23d6150f63798d0e2e62f59a227c58ffed581
Jiannine Torres <jiannine@agilebits.com>
2021-04-02 23:10:04 -0400

    Initial API call setup

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
+import pokeapi from "../api/pokeapi";
 
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
   const [query, setQuery] = useState("");
+  const [results, setResults] = useState({});
+
+  const searchQuery = async (name) => {
+    try {
+      const response = await pokeapi.get(`/pokemon/${name}`);
+      setResults(response.data);
+      console.log(response.data.name);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    searchQuery("pikachu");
+  }, []);
+
   return (
     <View>
       <Text>Search screen</Text>
       <SearchBar
         query={query}
         onQueryChange={setQuery}
-        onQuerySubmit={() => console.log(query)}
+        onQuerySubmit={() => searchQuery(query)}
       />
     </View>
   );
