1b6ea32ee96f4b070127a0e7f8fa8eb6378c27ab
Jiannine Torres <jiannine@agilebits.com>
2021-04-04 16:47:39 -0400

    Create Image component

---
+++ src/components/PokemonImage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Image, StyleSheet } from "react-native";
+
+const PokemonImage = ({ name }) => {
+  return (
+    <>
+      <Image
+        source={{
+          uri: `https://img.pokemondb.net/artwork/large/${name}.jpg`,
+        }}
+        style={styles.image}
+      />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: 100,
+    height: 120,
+    resizeMode: "contain",
+  },
+});
+
+export default PokemonImage;
