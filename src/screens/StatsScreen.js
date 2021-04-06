c100c7085c3feaec2eccc3bfc40fbc5cdef74151
Jiannine Torres <jiannine@agilebits.com>
2021-04-06 16:03:21 -0400

    Start showing data in Stats screen

--- src/screens/StatsScreen.js
+++ src/screens/StatsScreen.js
@@ -1,11 +1,35 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import useResults from "../hooks/useResults";
+import PokemonImage from "../components/PokemonImage";
 
 const StatsScreen = ({ navigation }) => {
   const name = navigation.getParam("name");
+  const [queryResults, searchQuery, queryErrorMessage] = useResults();
+
+  useEffect(() => {
+    searchQuery(name);
+  }, []);
+
   return (
     <View>
       <Text>Stats screen for {name}</Text>
+      <PokemonImage name={name} />
+      {queryResults ? (
+        <View>
+          <Text>{queryResults.id}</Text>
+          <View>
+            <Text>Types</Text>
+            <FlatList
+              data={queryResults.types}
+              keyExtractor={(result) => result.name}
+              renderItem={({ item }) => {
+                return <Text>{item.type.name}</Text>;
+              }}
+            />
+          </View>
+        </View>
+      ) : null}
     </View>
   );
 };
