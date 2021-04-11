ecdd5265fc0206963db83ea71b6faad059fa416e
Jiannine Torres <jiannine@agilebits.com>
2021-04-11 01:50:49 -0400

    Create types.js
    An object containing the background colours for each Pokemon type.

---
+++ src/utils/types.js
@@ -0,0 +1,23 @@
+const types = {
+  fire: "#FF924F",
+  water: "#6DBECF",
+  grass: "#94D474",
+  electric: "#FFE166",
+  psychic: "#5E6DD6",
+  ice: "#98D8D8",
+  dragon: "#8964E3",
+  dark: "#21214A",
+  fairy: "#FFC7D4",
+  normal: "#FFF5E0",
+  fighting: "#D94343",
+  flying: "#D4F2FF",
+  poison: "#9C5C9C",
+  ground: "#6B6454",
+  rock: "#9C9B98",
+  bug: "#A8B820",
+  ghost: "#574275",
+  steel: "#B8B8D0",
+  unknown: "#68A090",
+};
+
+export default types;
